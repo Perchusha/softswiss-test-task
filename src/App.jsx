95d11196f2ecb433c25a4f051f15c2d39242a932
Anton Perch <perch.anton@gmail.com>
2025-03-29 01:50:29 +0100

    feat: read more - done

--- src/App.jsx
+++ src/App.jsx
@@ -124,19 +124,24 @@ function App() {
 
       <section className="cta container">
         <span className="cta__title">Embark on a space journey</span>
-        <span className="cta__text">
-          Travelling into space is one of the most exciting and unforgettable adventures that can
-          change your life forever. And if you have ever dreamed of exploring stars, planets and
-          galaxies, then our company is ready to help you realize this dream. We offer a unique
-          experience that will allow you to go on a space journey and see all the secrets of the
-          universe. We guarantee that every moment in space will be filled with incredible
-          impressions, excitement and new discoveries. Our team of professionals takes care of your
-          safety and comfort so that you can fully enjoy your adventure in space. We offer various
-          options for space excursions.
-        </span>
-        <Link to="/journey" className="cta__link">
+        <input type="checkbox" id="toggle-cta" className="cta__checkbox" />
+
+        <div className="cta__text-wrapper">
+          <span className="cta__text">
+            Travelling into space is one of the most exciting and unforgettable adventures that can
+            change your life forever. And if you have ever dreamed of exploring stars, planets and
+            galaxies, then our company is ready to help you realize this dream. We offer a unique
+            experience that will allow you to go on a space journey and see all the secrets of the
+            universe. We guarantee that every moment in space will be filled with incredible
+            impressions, excitement and new discoveries. Our team of professionals takes care of
+            your safety and comfort so that you can fully enjoy your adventure in space. We offer
+            various options for space excursions.
+          </span>
+        </div>
+
+        <label htmlFor="toggle-cta" className="cta__button">
           Read more
-        </Link>
+        </label>
       </section>
 
       <footer className="footer">
