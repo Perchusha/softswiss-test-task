377c60ec62dfaf41e24a78592c5c28e328039436
Anton Perch <perch.anton@gmail.com>
2025-03-29 02:00:42 +0100

    feat: gh pages + routing

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
@@ -8,7 +8,10 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
+    <Router
+      future={{ v7_startTransition: true, v7_relativeSplatPath: true }}
+      basename="/softswiss-test-task"
+    >
       <App />
     </Router>
   </React.StrictMode>
